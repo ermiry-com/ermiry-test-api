6a6417a1188fb7e41230b2f935a052f4ff82a6aa
ermiry <erick.salas@ermiry.com>
2021-05-16 23:44:13 -0500

    Updated internal version methods definitions

--- src/version.c
+++ src/version.c
@@ -5,22 +5,40 @@
 // print full pocket version information
 void test_version_print_full (void) {
 
-	cerver_log_msg ("Test API Version: %s", TEST_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s", TEST_VERSION_DATE, TEST_VERSION_TIME);
-	cerver_log_msg ("Author: %s", TEST_VERSION_AUTHOR);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Test API Version: %s", TEST_VERSION_NAME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s", TEST_VERSION_DATE, TEST_VERSION_TIME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n", TEST_VERSION_AUTHOR
+	);
+
 
 }
 
 // print the version id
 void test_version_print_version_id (void) {
 
-	cerver_log_msg ("Test API Version ID: %s", TEST_VERSION);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Test API Version ID: %s\n", TEST_VERSION
+	);
 
 }
 
 // print the version name
 void test_version_print_version_name (void) {
 
-	cerver_log_msg ("Test API Version: %s", TEST_VERSION_NAME);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Test API Version: %s\n", TEST_VERSION_NAME
+	);
 
 }
