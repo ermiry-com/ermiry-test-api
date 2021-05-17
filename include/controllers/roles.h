2a9c31bff65e7bc299e15311bbff95e53c3be0d9
ermiry <erick.salas@ermiry.com>
2021-05-16 23:41:45 -0500

    Added more methods in role controller

--- include/controllers/roles.h
+++ include/controllers/roles.h
@@ -1,5 +1,5 @@
-#ifndef _TEST_CONTROLLERS_ROLES_H_
-#define _TEST_CONTROLLERS_ROLES_H_
+#ifndef _TEST_ROLES_H_
+#define _TEST_ROLES_H_
 
 #include <bson/bson.h>
 
@@ -11,7 +11,15 @@ extern unsigned int test_roles_init (void);
 
 extern void test_roles_end (void);
 
-extern const char *test_roles_get_by_oid (
+extern const Role *test_role_get_by_oid (
+	const bson_oid_t *role_oid
+);
+
+extern const Role *test_role_get_by_name (
+	const char *role_name
+);
+
+extern const char *test_role_name_get_by_oid (
 	const bson_oid_t *role_oid
 );
 
