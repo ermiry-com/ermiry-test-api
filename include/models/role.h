c21c8f56bd94078e03a4b4996f39652b6b9ab84a
ermiry <erick.salas@ermiry.com>
2021-07-05 18:34:32 -0500

    Updated main role model implementation

--- include/models/role.h
+++ include/models/role.h
@@ -4,10 +4,13 @@
 #include <bson/bson.h>
 #include <mongoc/mongoc.h>
 
+#include <cerver/collections/dlist.h>
+
 #include <cmongo/select.h>
 
 #define ROLES_COLL_NAME  		"roles"
 
+#define ROLE_ID_SIZE			32
 #define ROLE_NAME_SIZE			128
 #define ROLE_ACTIONS_SIZE		32
 #define ROLE_ACTION_SIZE		64
@@ -19,6 +22,7 @@ extern void roles_model_end (void);
 struct _Role {
 
 	bson_oid_t oid;
+	char id[ROLE_ID_SIZE];
 
 	char name[ROLE_NAME_SIZE];
 
@@ -33,52 +37,14 @@ extern Role *role_new (void);
 
 extern void role_delete (void *role_ptr);
 
-extern Role *role_create (
-	const char *name
+extern int role_comparator_by_name (
+	const void *a, const void *b
 );
 
-extern void role_print (
-	Role *role
-);
+extern void role_print (const Role *role);
 
-// creates a role bson with all role parameters
-extern bson_t *role_bson_create (
-	Role *role
-);
-
-extern void role_doc_parse (
-	void *role_ptr, const bson_t *role_doc
-);
-
-extern unsigned int role_get_by_oid (
-	Role *role, const bson_oid_t *oid, const bson_t *query_opts
-);
-
-extern unsigned int role_get_by_cuc (
-	Role *role,
-	const char *cuc, const bson_t *query_opts
-);
-
-// gets a role form the db by its name
-// option to select if you want actions or not
-extern Role *role_get_by_name (
-	const char *name, bool actions
-);
-
-extern bson_t *role_bson_create_oid_query (
-	const bson_oid_t *oid
-);
-
-extern bson_t *role_bson_create_name_query (
-	const char *name
-);
-
-extern bson_t *role_bson_create_update (
-	Role *role
-);
-
-extern mongoc_cursor_t *role_find_all (
-	const CMongoSelect *select, uint64_t *n_docs
+extern DoubleList *roles_get_all (
+	const CMongoSelect *select
 );
 
 #endif
